c356ffc3e8a24c4688f55f3cecf2d8a2375fa5b2
neoluigi4123 <neoluigimario2008@gmail.com>
2026-02-23 17:46:50 +0100

    fixed typing error with context

--- llm.py
+++ llm.py
@@ -2,13 +2,14 @@
 This module defines the llm class, which serves as a wrapper around the Mistral API for generating responses based on prompts. It provides a simple interface for interacting with the Mistral language model.
 """
 import config
+from typing import Any
 from mistralai import Mistral
 
 class llm:
     def __init__(self, model: str, api_key: str = config.MISTRAL_API_KEY):
         self.model = model
         self.client = Mistral(api_key=api_key)
-        self.context: dict = [
+        self.context: list[Any] = [
             {
                 "role": "system",
                 "content": "You are a helpful assistant that provides accurate and concise information to the user.",
@@ -21,8 +22,9 @@ class llm:
         Returns:
             str: The generated response from the LLM.
         """
+        self.context.append({"role": "user", "content": prompt})
         chat_response = self.client.chat.complete(
             model = self.model,
-            messages = self.context + [{"role": "user", "content": prompt}]
+            messages = self.context
         )
         return chat_response.choices[0].message.content
