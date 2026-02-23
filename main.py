8e8b899028e67e86ba976a207e0fa7108546b23a
Poutrashkov-afk <poutrashkov@gmail.com>
2026-02-23 19:59:55 +0100

    added context loading on boot

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 import config
 import llm
+import json
 
 assistant = llm.llm(model=config.DEFAULT_MODEL)
+try:
+    with open("local_data/context.json", "r") as file:
+        previous_context = json.load(file)
+except Exception as e:
+    print(f"Error reading context: {e}")
+
 while True:
     prompt = input("> ")
     
