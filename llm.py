bceda68a1e759c756f8927062afb598dbe7cd51f
neoluigi4123 <neoluigimario2008@gmail.com>
2026-02-23 16:22:11 +0100

    main.py updated

--- llm.py
+++ llm.py
@@ -1,17 +1,14 @@
 import config
 from mistralai import Mistral
 
-api_key = config.MISTRAL_API_KEY
-model = "mistral-large-latest"
-
-client = Mistral(api_key=api_key)
 class llm:
-    def __init__(self):
-        pass
+    def __init__(self, model: str, api_key: str = config.MISTRAL_API_KEY):
+        self.model = model
+        self.client = Mistral(api_key=api_key)
     
     def generate(self, prompt):
-        chat_response = client.chat.complete(
-            model = model,
+        chat_response = self.client.chat.complete(
+            model = self.model,
             messages = [
                 {
                     "role": "user",
--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 import config
 import llm
 
+assistant = llm.llm(model="mistral-large-latest")
+
+response = assistant.generate("What is the capital of France?")
+
+print(response)
