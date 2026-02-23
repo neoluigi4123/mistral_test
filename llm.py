d659f0e18ba4a0df785789b3c9ce5ef6d059c521
neoluigi4123 <neoluigimario2008@gmail.com>
2026-02-23 17:39:37 +0100

    fixed issue with dictionnary in llm.py about context

--- llm.py
+++ llm.py
@@ -8,13 +8,13 @@ class llm:
     def __init__(self, model: str, api_key: str = config.MISTRAL_API_KEY):
         self.model = model
         self.client = Mistral(api_key=api_key)
-        self.context = [
+        self.context: dict = [
             {
                 "role": "system",
                 "content": "You are a helpful assistant that provides accurate and concise information to the user.",
             }
         ]
-    def generate(self, prompt):
+    def generate(self, prompt: str):
         """Generates a response from the LLM based on the given prompt.
         Args:
             prompt (str): The input prompt to generate a response for.
