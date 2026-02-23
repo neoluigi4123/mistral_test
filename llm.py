e85f6c389de0ec316734f9aa404dbaf7b6e63dc3
Poutrashkov-afk <poutrashkov@gmail.com>
2026-02-23 17:38:24 +0100

    added context to llm

--- llm.py
+++ llm.py
@@ -8,7 +8,12 @@ class llm:
     def __init__(self, model: str, api_key: str = config.MISTRAL_API_KEY):
         self.model = model
         self.client = Mistral(api_key=api_key)
-    
+        self.context = [
+            {
+                "role": "system",
+                "content": "You are a helpful assistant that provides accurate and concise information to the user.",
+            }
+        ]
     def generate(self, prompt):
         """Generates a response from the LLM based on the given prompt.
         Args:
@@ -18,11 +23,6 @@ class llm:
         """
         chat_response = self.client.chat.complete(
             model = self.model,
-            messages = [
-                {
-                    "role": "user",
-                    "content": prompt,
-                },
-            ]
+            messages = self.context + [{"role": "user", "content": prompt}]
         )
         return chat_response.choices[0].message.content
