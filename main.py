6a61e0659a310a184d531928fb072bcdda116498
neoluigi4123 <neoluigimario2008@gmail.com>
2026-02-23 20:05:54 +0100

    fixed previous convo loading (with else statement)

--- main.py
+++ main.py
@@ -14,9 +14,11 @@ except Exception as e:
     print(f"Error reading context: {e}")
 
 if previous_context:
-        print("Previous context loaded...")
-        for messages in previous_context:
-            assistant.add_to_context(messages["role"], messages["content"])
+    print("Previous context loaded...")
+    for messages in previous_context:
+        assistant.add_to_context(messages["role"], messages["content"])
+else:
+    print("No previous context found. Starting fresh...")
 
 while True:
     prompt = input("> ")
