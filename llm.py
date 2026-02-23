ab33474bc21e5b7d860f6fee16e47f2d601e5a9c
neoluigi4123 <neoluigimario2008@gmail.com>
2026-02-23 15:54:32 +0100

    Updated llm.py with class

--- llm.py
+++ llm.py
@@ -1,4 +1,3 @@
-import os
 import config
 from mistralai import Mistral
 
@@ -6,15 +5,18 @@ api_key = config.MISTRAL_API_KEY
 model = "mistral-large-latest"
 
 client = Mistral(api_key=api_key)
-
-def generate():
-    chat_response = client.chat.complete(
-        model = model,
-        messages = [
-            {
-                "role": "user",
-                "content": "Tell me a joke.",
-            },
-        ]
-    )
-    return chat_response.choices[0].message.content
+class llm:
+    def __init__(self):
+        pass
+    
+    def generate(self, prompt):
+        chat_response = client.chat.complete(
+            model = model,
+            messages = [
+                {
+                    "role": "user",
+                    "content": prompt,
+                },
+            ]
+        )
+        return chat_response.choices[0].message.content
--- main.py
+++ main.py
@@ -1,20 +1,3 @@
-import os
 import config
-from mistralai import Mistral
+import llm
 
-api_key = config.MISTRAL_API_KEY
-model = "mistral-large-latest"
-
-client = Mistral(api_key=api_key)
-
-chat_response = client.chat.complete(
-    model = model,
-    messages = [
-        {
-            "role": "user",
-            "content": "Tell me a joke.",
-        },
-    ]
-)
-
-print(chat_response.choices[0].message.content)
