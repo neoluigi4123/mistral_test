850b7dac657d803ec3ea1d17b180bfda5de85989
neoluigi4123 <neoluigimario2008@gmail.com>
2026-02-23 19:09:01 +0100

    updated main.py

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ assistant = llm.llm(model=config.DEFAULT_MODEL)
 while True:
     response = assistant.generate(input("> "))
 
-    print("assistant:\n",response)
+    print(f"assistant:\n{response}")
